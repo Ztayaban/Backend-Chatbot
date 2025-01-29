2ba9f8919585d13f499a5d2b9b0770ffe3a7e3ef
Ztayaban <pinkihanzaidee@gmail.com>
2025-01-29 16:23:31 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from api.chat import get_response
+import nltk
+nltk.download('punkt')
+
 
 app = Flask(__name__)
 CORS(app)
